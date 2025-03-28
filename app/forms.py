b47d6ac605190a95c5d98f7cd7b51f02ea48f375
abegailmccalla <abegailtatyanamccalla7@gmail.com>
2025-03-28 09:56:44 -0500

    Exercise 1 Complete

--- app/forms.py
+++ app/forms.py
@@ -1,23 +1,11 @@
 # Add any form classes for Flask-WTF here
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, FileField
-from wtforms.validators import DataRequired, Length
-from flask_wtf.file import FileAllowed
+from wtforms.validators import InputRequired, DataRequired, Length
+from flask_wtf.file import FileRequired, FileField, FileAllowed
 
 class MovieForm(FlaskForm):
-    # Title field: A string field with a DataRequired validator
-    title = StringField('Movie Title', validators=[
-        DataRequired(message="The title is required"),
-        Length(max=100, message="The title cannot exceed 100 characters")
-    ])
+    title = StringField('Title', validators=[InputRequired()])
+    description = TextAreaField('Decription', validators=[InputRequired()])
+    poster = FileField('Poster', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'or', 'jpeg', 'images only!'])])
     
-    # Description field: A text area field with a DataRequired validator
-    description = TextAreaField('Movie Description', validators=[
-        DataRequired(message="A brief description is required")
-    ])
-    
-    # Poster field: A file field that only allows image uploads
-    poster = FileField('Movie Poster', validators=[
-        FileAllowed(['jpg', 'png'], "Only image files (JPG or PNG) are allowed")
-    ])
--- app/models.py
+++ app/models.py
@@ -1,1 +1,43 @@
 # Add any model classes for Flask-SQLAlchemy here
+
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+from app import db
+from datetime import datetime
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    poster = db.Column(db.String(255), nullable=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    #created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, id, title, description, poster, created_at):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.poster = poster
+        self.created_at = created_at
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
